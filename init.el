e63754716cc7fa7579e77d72b7458505510b77e8
Jiacheng Huang <huangjiacheng0709@outlook.com>
2020-10-21 00:12:07 +0800

    Set up Helm

--- init.el
+++ init.el
@@ -14,13 +14,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(flycheck company-irony irony company auto-complete smartparens dracula-theme)))
+   '(helm flycheck company-irony irony company auto-complete smartparens dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Helm
+(require 'helm-config)
+(helm-mode 1)
 
 ;; Irony
 (add-hook 'c++-mode-hook 'irony-mode)
