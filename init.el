cac02a3649b7dd347ce98ac850fd4662b1fc7f19
Jiacheng Huang <huangjiacheng0709@outlook.com>
2020-10-27 10:13:32 +0800

    Set up LSP Mode

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 (require 'package)
-;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
 ;; and `package-pinned-packages`. Most users will not need or want to do this.
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (custom-set-variables
@@ -11,7 +11,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(counsel ivy flycheck company smartparens dracula-theme)))
+   '(lsp-mode counsel ivy flycheck company smartparens dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -35,6 +35,10 @@
 
 (setq company-idle-delay 0)
 (setq company-minimum-prefix-length 2)
+
+;; LSP
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
 
 ;; Flycheck
 (add-hook 'c++-mode-hook 'flycheck-mode)
