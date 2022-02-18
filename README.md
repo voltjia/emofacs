989c5c83338d053d8f5603a30cfa4bfb8a0fd73a
Jiacheng Huang <huangjiacheng0709@outlook.com>
2022-02-17 23:55:33 -0600

    Refactor with straight and use-package

--- README.md
+++ README.md
@@ -18,34 +18,38 @@ A language server is only necessary when you want to code in a specific programm
 
 ## Installation
 
-For the convenience to configure an empty Emacs as simple as possible, most packages in my configuration are contained in the MELPA package manager, so the only file you really need is `init.el`.
+For the convenience to configure an empty Emacs as simple as possible, the straight package manager (straight.el) and use-package are used, and the only file you really need is `init.el`.
 
 Simply put `init.el` into `.emacs.d`, the standard location for additional per-user Emacs-specific files, Emacs is going to configure itself automatically during the next startup. Or if you want, you may git clone this whole repository as your `.emacs.d`. In this way, you may update this configuration using git in the future. For more information about `.emacs.d`, please visit https://www.emacswiki.org/emacs/DotEmacsDotD.
 
-The installation time depends on your Internet connection, because the packages need to be downloaded. It takes about 5 minutes on my personal computer at home. During installation, Emacs may seem to be stuck, but it is not, so just wait patiently, but if the installation is not complete after 20 minutes, then there might be something wrong. In order to use `all-the-icons` package, Emacs may prompt you to install `all-the-icons` fonts if none detected, type `yes` when prompted, or you may choose to disable features using `all-the-icons`, although in this way Emofacs may look less pretty.
+The installation time depends on your Internet connection and CPU, because the packages need to be cloned and built. It takes about 3 minutes on my personal computer at home. During installation, Emacs may seem to be stuck, but it is not, so just wait patiently, but if the installation is not complete after 20 minutes, then there might be something wrong. In order to use `all-the-icons` package, Emacs may prompt you to install `all-the-icons` fonts if none detected, type `yes` when prompted, or you may choose to disable features using `all-the-icons`, although in this way Emofacs may look less pretty.
 
 Even if everything goes well, Emacs may still display some warnings and errors after initialization, and the `all-the-icons` theme may not be loaded the first time Emacs starts up. Just ignore them and restart Emacs, and then Emofacs should work properly.
 
 ## Packages
 
-* [use-package](https://github.com/jwiegley/use-package) (allows you to isolate package configuration in a way that is both performance-oriented and, well, tidy)
-* [exec-path-from-shell](https://github.com/purcell/exec-path-from-shell) (ensures environment variables inside Emacs look the same as in the user's shell)
-* [Dracula](https://draculatheme.com/emacs/) (dark theme)
-* [all-the-icons](https://github.com/domtronn/all-the-icons.el) (used to include fancy icons)
-* [Power Line](https://github.com/milkypostman/powerline) (Emacs version of the Vim powerline)
-* [Dashboard](https://github.com/emacs-dashboard/emacs-dashboard) (a startup dashboard which provides certain information about your recent Emacs activities)
-* [Zoom](https://github.com/cyrus-and/zoom) (fixed and automatic balanced window layout for Emacs)
-* [Ivy](https://github.com/abo-abo/swiper) (generic completion mechanism)
-* [Projectile](https://github.com/bbatsov/projectile) (project interaction library)
-* [Treemacs](https://github.com/Alexander-Miller/treemacs) (tree layout file explorer)
-* [Company](https://company-mode.github.io/) (text completion framework)
-* [LSP](https://emacs-lsp.github.io/lsp-mode/) (language server protocol)
-* [YASnippet](https://github.com/joaotavora/yasnippet) (template system)
-* [Flycheck](https://www.flycheck.org/en/latest/) (modern on-the-fly syntax checking extension)
-* [Smartparens](https://github.com/Fuco1/smartparens) (dealing with pairs)
-* [Magit](https://magit.vc/) (complete text-based user interface to [Git](https://git-scm.com/))
+Here is a list of packages used in Emofacs:
 
-They are the external packages and their links.
+* [ace-window](https://github.com/abo-abo/ace-window) - Quickly switch windows.
+* [all-the-icons](https://github.com/domtronn/all-the-icons.el) - A package used to include fancy icons within emacs.
+* [Company](https://company-mode.github.io/) - A text completion framework.
+* [Dashboard](https://github.com/emacs-dashboard/emacs-dashboard) - A startup dashboard which provides certain information about your recent Emacs activities.
+* [exec-path-from-shell](https://github.com/purcell/exec-path-from-shell) - Get environment variables such as $PATH from the shell for Mac user.
+* [Flycheck](https://www.flycheck.org/en/latest/) - Modern on-the-fly syntax checking meant to be a replacement to FlyMake.
+* [Iedit](https://github.com/victorhge/iedit) - Modify multiple occurrences in the same way simultaneously.
+* [Ivy](https://github.com/abo-abo/swiper) - Flexible and simple tools for minibuffer completion in Emacs.
+* [LSP](https://emacs-lsp.github.io/lsp-mode/) - Emacs client for the Language Server Protocol.
+* [Magit](https://magit.vc/) - Interacting with git.
+* [powerline](https://github.com/milkypostman/powerline) - Emacs version of the Vim powerline.
+* [Projectile](https://github.com/bbatsov/projectile) - Project Interaction Library for Emacs.
+* [Smartparens](https://github.com/Fuco1/smartparens) - Deals with parens pairs and tries to be smart about it.
+* [straight](https://github.com/raxod502/straight.el) - Next-generation, purely functional package manager for the Emacs hacker.
+* [Treemacs](https://github.com/Alexander-Miller/treemacs) - A tree layout file explorer for Emacs.
+* [use-package](https://github.com/jwiegley/use-package) - A declaration macro to isolate package configuration in a way that is performance-oriented and tidy.
+* [which-key](https://github.com/justbur/emacs-which-key) - Display available key bindings in popup. Rewrite of guide-key with added features to improve display.
+* [Zenburn](https://github.com/bbatsov/zenburn-emacs) - Vim’s “Zenburn theme” ported to Emacs.
+
+You may also want to find some interesting packages yourself. Then this community driven list of useful Emacs packages, libraries, and others - [Awesome Emacs](https://github.com/emacs-tw/awesome-emacs) - might be helpful.
 
 ## Usage
 
@@ -56,4 +60,3 @@ However, I am not going to teach you how to use Emacs or any of the specific pac
 ## License
 
 MIT License © Jiacheng Huang
-
--- init.el
+++ init.el
@@ -1,276 +1,223 @@
-;; Copyright (c) 2020-2021 Jiacheng Huang
+;;; straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; Permission is hereby granted, free of charge, to any person obtaining a copy
-;; of this software and associated documentation files (the "Software"), to deal
-;; in the Software without restriction, including without limitation the rights
-;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-;; copies of the Software, and to permit persons to whom the Software is
-;; furnished to do so, subject to the following conditions:
+;;; use-package
+(straight-use-package 'use-package)
 
-;; The above copyright notice and this permission notice shall be included in
-;; all copies or substantial portions of the Software.
-
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-;; SOFTWARE.
-
-;; Package Setup
-(setq package-archives '(("melpa" . "http://melpa.org/packages/")
-                         ("gnu" . "http://elpa.gnu.org/packages/")))
-
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" default))
- '(package-selected-packages
-   '(lsp-haskell lsp-pyright zenburn-theme ido-completing-read+ iedit lsp-ivy lsp-ui latex-preview-pane counsel-projectile powerline zoom treemacs-all-the-icons org-dashboard dashboard lsp-java dap-mode which-key magit go-mode exec-path-from-shell rustic rust-mode yasnippet-snippets yasnippet treemacs-projectile treemacs projectile lsp-mode counsel ivy flycheck company smartparens)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(when (cl-find-if-not #'package-installed-p package-selected-packages)
-  (package-refresh-contents)
-  (mapc #'package-install package-selected-packages))
-
-;; Bootstrap John Wigley's "use-package"
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(require 'use-package)
-
-;; Ensure environment variables inside Emacs look the same as in the user's shell
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
-(when (daemonp)
+;;; exec-path-from-shell
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :straight t
+  :config
   (exec-path-from-shell-initialize))
 
-;; Zenburn
-(load-theme 'zenburn t)
+;;; Zenburn
+(use-package zenburn-theme
+  :straight t
+  :config
+  (load-theme 'zenburn t))
 
-;; All The Icons
-(require 'all-the-icons)
-(when (display-graphic-p)
-  (unless (member "all-the-icons" (font-family-list)) (all-the-icons-install-fonts)))
+;;; all-the-icons
+(use-package all-the-icons
+  :straight t
+  :if (display-graphic-p)
+  :config
+  (unless (member "all-the-icons" (font-family-list))
+    (all-the-icons-install-fonts)))
 
-;; Power Line
-(require 'powerline)
-(powerline-default-theme)
+;;; powerline
+(use-package powerline
+  :straight t
+  :config
+  (powerline-default-theme))
 
-;; Dashboard
-(require 'dashboard)
-(dashboard-setup-startup-hook)
-(setq dashboard-banner-logo-title "E + mofa + cs")
-(setq dashboard-center-content t)
-(setq dashboard-items '((projects . 8)
-                        (recents . 8)))
-(setq dashboard-startup-banner 'logo)
-(setq dashboard-set-heading-icons t)
-(setq dashboard-set-file-icons t)
-(setq dashboard-set-navigator t)
-(setq dashboard-set-init-info t)
-(setq dashboard-projects-switch-function 'counsel-projectile-switch-project-by-name)
+;;; Dashboard
+(use-package dashboard
+  :straight t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-banner-logo-title "E + mofa + cs")
+  (setq dashboard-startup-banner 'logo)
+  (setq dashboard-center-content t)
+  (setq dashboard-items '((projects . 8)
+                          (recents . 8)))
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-set-navigator t)
+  (setq dashboard-set-init-info t))
 
-;; Zoom
-(zoom-mode)
-(setq zoom-size '(0.618 . 0.618))
+;;; Projectile
+(use-package projectile
+  :straight t
+  :config
+  (projectile-mode 1)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
-;; Projectile
-(projectile-mode 1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+;;; Iedit
+(use-package iedit
+  :straight t)
 
-;; Iedit
-(require 'iedit)
+;;; Ivy
+(use-package ivy
+  :straight t
+  :config
+  (ivy-mode)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t))
 
-;; Ido
-(ido-mode 1)
-(ido-everywhere 1)
+;;; Swiper
+(use-package swiper
+  :straight t
+  :after (ivy)
+  :config
+  (global-set-key "\C-s" 'swiper))
 
-;; Ido Completing Read Plus
-(require 'ido-completing-read+)
-(ido-ubiquitous-mode 1)
+;;; Counsel
+(use-package counsel
+  :straight t
+  :after (swiper)
+  :config
+  (counsel-mode))
 
-;; Ivy
-(ivy-mode)
-(counsel-mode)
-(counsel-projectile-mode)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-(setq search-default-mode #'char-fold-to-regexp)
-(global-set-key "\C-s" 'swiper)
+;;; Treemacs
+(use-package treemacs
+  :straight t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (setq treemacs-show-cursor t
+        treemacs-width 32)
+  (treemacs-filewatch-mode t)
+  (treemacs-fringe-indicator-mode t)
+  :bind
+  (:map global-map
+        ("M-0" . treemacs-select-window)))
 
-;; Treemacs
-(require 'treemacs)
-(treemacs-follow-mode t)
-(treemacs-filewatch-mode t)
-(treemacs-fringe-indicator-mode t)
-(setq treemacs-show-cursor t)
-(setq treemacs-width 32)
-(define-key treemacs-mode-map [mouse-1] #'treemacs-single-click-expand-action)
-(define-key global-map (kbd "M-0") 'treemacs-select-window)
+;;; treemacs-projectile
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :straight t)
 
-;; Treemacs All The Icons
-(require 'treemacs-all-the-icons)
-(treemacs-load-theme "all-the-icons")
+;;; treemacs-icons-dired
+(use-package treemacs-icons-dired
+  :hook (dired-mode . treemacs-icons-dired-enable-once)
+  :straight t)
 
-;; Treemacs Projectile
-(require 'treemacs-projectile)
+;;; treemacs-magit
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :straight t)
 
-;; Company
-(add-hook 'after-init-hook 'global-company-mode)
+;;; treemacs-all-the-icons
+(use-package treemacs-all-the-icons
+  :after (treemacs all-the-icons)
+  :straight t
+  :config
+  (treemacs-load-theme "all-the-icons"))
 
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 1)
-(setq company-show-numbers t)
+;;; Company
+(use-package company
+  :straight t
+  :hook (after-init . global-company-mode)
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1)
+  (setq company-show-numbers t))
 
-;; Which Key
-(which-key-mode)
+;;; which-key
+(use-package which-key
+  :straight t
+  :config
+  (which-key-mode))
 
-;; YASnippet
-(yas-global-mode 1)
+;;; LSP
+(use-package lsp-mode
+  :straight t
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((c-mode . lsp)
+         (c++-mode . lsp)
+         (go-mode . lsp)
+         (haskell-mode . lsp)
+         (haskell-literate-mode . lsp)
+         (java-mode . lsp)
+         (js-mode . lsp)
+         (python-mode . lsp)
+         (rust-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
 
-(defvar my-company-point nil)
-(advice-add 'company-complete-common
-            :before (lambda () (setq my-company-point (point))))
-(advice-add 'company-complete-common
-            :after (lambda ()
-  		     (when (equal my-company-point (point))
-  		       (yas-expand))))
+;;; lsp-ui
+(use-package lsp-ui
+  :straight t
+  :commands lsp-ui-mode)
 
-;; LSP
-(add-hook 'c-mode-hook #'lsp)
-(add-hook 'c++-mode-hook #'lsp)
-(add-hook 'go-mode-hook #'lsp)
-(require 'lsp-java)
-(add-hook 'java-mode-hook #'lsp)
-(add-hook 'js-mode-hook #'lsp)
-(use-package lsp-pyright
-  :ensure t
-  :hook (python-mode . (lambda ()
-                         (require 'lsp-pyright)
-                         (lsp))))
-(add-hook 'rust-mode-hook #'lsp)
-(add-hook 'latex-mode-hook #'lsp)
-(add-hook 'markdown-mode-hook #'lsp)
-(require 'lsp-haskell)
-(add-hook 'haskell-mode-hook #'lsp)
-(add-hook 'haskell-literate-mode-hook #'lsp)
-(setq lsp-clients-clangd-args
-      '("--header-insertion=never"
-        "--header-insertion-decorators=0"))
-(with-eval-after-load 'lsp-mode
-  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
-  (require 'dap-cpptools)
-  (yas-global-mode))
+;;; lsp-ivy
+(use-package lsp-ivy
+  :straight t
+  :commands lsp-ivy-workspace-symbol)
 
-;; Flycheck
-(add-hook 'c-mode-hook #'flycheck-mode)
-(add-hook 'c++-mode-hook #'flycheck-mode)
-(add-hook 'go-mode-hook #'flycheck-mode)
-(add-hook 'java-mode-hook #'flycheck-mode)
-(add-hook 'js-mode-hook #'flycheck-mode)
-(add-hook 'python-mode-hook #'flycheck-mode)
-(add-hook 'rust-mode-hook #'flycheck-mode)
+;;; lsp-treemacs
+(use-package lsp-treemacs
+  :straight t
+  :commands lsp-treemacs-errors-list)
 
-;; Smartparens
-(require 'smartparens-config)
-(add-hook 'asm-mode-hook #'smartparens-mode)
-(add-hook 'c-mode-hook #'smartparens-mode)
-(add-hook 'c++-mode-hook #'smartparens-mode)
-(add-hook 'emacs-lisp-mode-hook #'smartparens-mode)
-(add-hook 'go-mode-hook #'smartparens-mode)
-(add-hook 'java-mode-hook #'smartparens-mode)
-(add-hook 'js-mode-hook #'smartparens-mode)
-(add-hook 'python-mode-hook #'smartparens-mode)
-(add-hook 'rust-mode-hook #'smartparens-mode)
-(add-hook 'rustic-mode-hook #'smartparens-mode)
-(add-hook 'verilog-mode #'smartparens-mode)
+;;; Flycheck
+(use-package flycheck
+  :straight t
+  :init (global-flycheck-mode))
 
-;; When you press RET, the curly braces automatically add another newline.
-(sp-with-modes '(asm-mode c-mode c++-mode emacs-lisp-mode java-mode js-mode python-mode rust-mode rustic-mode verilog-mode)
-  (sp-local-pair "{" nil :post-handlers '(("||\n[i]" "RET")))
-  (sp-local-pair "/*" "*/" :post-handlers '(("| " "SPC") ("* ||\n[i]" "RET"))))
-(setq sp-escape-quotes-after-insert nil)
+;;; Smartparens
+(use-package smartparens-config
+  :straight smartparens
+  :config
+  (smartparens-global-mode t)
+  (show-smartparens-global-mode t))
 
-;; Windmove
-(when (fboundp 'windmove-default-keybindings)
-  (windmove-default-keybindings))
+;;; ace-window
+(use-package ace-window
+  :straight t
+  :config
+  (global-set-key (kbd "M-o") 'ace-window))
 
-;; Ace Window
-(global-set-key (kbd "M-o") 'ace-window)
-
-;; Winner
-(when (fboundp 'winner-mode)
-  (winner-mode 1))
-
-;; Alarm Bell
+;; Turn off alarms completely.
 (setq ring-bell-function 'ignore)
 
-;; Window Resize
+;; Keybindings to resize windows.
 (global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
 
-;; Line Number Mode
-(if (version<= "26.0.50" emacs-version)
-    (global-display-line-numbers-mode)
-  (global-linum-mode 1))
+;; Display line numbers.
+(when (version<= "26.0.50" emacs-version)
+  (global-display-line-numbers-mode))
 
-;; Column Number Mode
-(column-number-mode 1)
+;; Display the current column number.
+(setq column-number-mode t)
 
-;; Start Emacs in fullscreen mode
+;; Start Emacs in fullscreen mode.
 (add-hook 'emacs-startup-hook 'toggle-frame-fullscreen)
 
-;; Place auto-save files into system's temporary file
+;; Place auto-save files to a dedicated directory.
 (setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
+      `(("." . ,(concat user-emacs-directory "backups"))))
 
-;; Delete Selection Mode
+;; Replace highlighted text with what I type.
 (delete-selection-mode 1)
 
-;; No Tabs
+;; Use spaces instead of tabs when indenting.
 (setq-default indent-tabs-mode nil)
-
-;; Org Mode
-(eval-after-load "org"
-  '(require 'ox-md nil t))
-(setq org-adapt-indentation nil)
-
-;; CC Mode
-(require 'cc-mode)
-(setq c-default-style "user")
-
-;; Rust Mode
-(require 'rust-mode)
-(setq rust-format-on-save t)
-(define-key rust-mode-map (kbd "C-c C-c") 'rust-run)
-
-;; Rustic Mode
-(use-package rustic)
-
-;; JavaScript Mode
-(setq-default js-indent-level 2)
-
-;; CSS Mode
-(setq-default css-indent-offset 2)
