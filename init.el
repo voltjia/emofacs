20c360ed94e4644c726f4e58c0b2ae2fe6710e37
Jiacheng Huang <huangjiacheng0709@outlook.com>
2022-02-18 11:09:05 -0600

    Reorganize packages in logical order

--- init.el
+++ init.el
@@ -22,53 +22,6 @@
   :config
   (exec-path-from-shell-initialize))
 
-;;; Zenburn
-(use-package zenburn-theme
-  :straight t
-  :config
-  (load-theme 'zenburn t))
-
-;;; all-the-icons
-(use-package all-the-icons
-  :straight t
-  :if (display-graphic-p)
-  :config
-  (unless (member "all-the-icons" (font-family-list))
-    (all-the-icons-install-fonts)))
-
-;;; powerline
-(use-package powerline
-  :straight t
-  :config
-  (powerline-default-theme))
-
-;;; Dashboard
-(use-package dashboard
-  :straight t
-  :config
-  (dashboard-setup-startup-hook)
-  (setq dashboard-banner-logo-title "E + mofa + cs")
-  (setq dashboard-startup-banner 'logo)
-  (setq dashboard-center-content t)
-  (setq dashboard-items '((projects . 8)
-                          (recents . 8)))
-  (setq dashboard-set-heading-icons t)
-  (setq dashboard-set-file-icons t)
-  (setq dashboard-set-navigator t)
-  (setq dashboard-set-init-info t))
-
-;;; Projectile
-(use-package projectile
-  :straight t
-  :config
-  (projectile-mode 1)
-  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
-
-;;; Iedit
-(use-package iedit
-  :straight t)
-
 ;;; Ivy
 (use-package ivy
   :straight t
@@ -91,6 +44,47 @@
   :config
   (counsel-mode))
 
+;;; which-key
+(use-package which-key
+  :straight t
+  :config
+  (which-key-mode))
+
+;;; ace-window
+(use-package ace-window
+  :straight t
+  :config
+  (global-set-key (kbd "M-o") 'ace-window))
+
+;;; Company
+(use-package company
+  :straight t
+  :hook (after-init . global-company-mode)
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1)
+  (setq company-show-numbers t))
+
+;;; Iedit
+(use-package iedit
+  :straight t)
+
+;;; Projectile
+(use-package projectile
+  :straight t
+  :config
+  (projectile-mode 1)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+
+;;; all-the-icons
+(use-package all-the-icons
+  :straight t
+  :if (display-graphic-p)
+  :config
+  (unless (member "all-the-icons" (font-family-list))
+    (all-the-icons-install-fonts)))
+
 ;;; Treemacs
 (use-package treemacs
   :straight t
@@ -108,55 +102,44 @@
 
 ;;; treemacs-projectile
 (use-package treemacs-projectile
-  :after (treemacs projectile)
-  :straight t)
+  :straight t
+  :after (treemacs projectile))
 
 ;;; treemacs-icons-dired
 (use-package treemacs-icons-dired
-  :hook (dired-mode . treemacs-icons-dired-enable-once)
-  :straight t)
+  :straight t
+  :hook (dired-mode . treemacs-icons-dired-enable-once))
 
 ;;; treemacs-magit
 (use-package treemacs-magit
-  :after (treemacs magit)
-  :straight t)
+  :straight t
+  :after (treemacs magit))
 
 ;;; treemacs-all-the-icons
 (use-package treemacs-all-the-icons
-  :after (treemacs all-the-icons)
   :straight t
+  :after (treemacs all-the-icons)
   :config
   (treemacs-load-theme "all-the-icons"))
 
-;;; Company
-(use-package company
-  :straight t
-  :hook (after-init . global-company-mode)
+;;; Smartparens
+(use-package smartparens-config
+  :straight smartparens
   :config
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 1)
-  (setq company-show-numbers t))
+  (smartparens-global-mode t)
+  (show-smartparens-global-mode t))
 
-;;; which-key
-(use-package which-key
+;;; Flycheck
+(use-package flycheck
   :straight t
-  :config
-  (which-key-mode))
+  :init (global-flycheck-mode))
 
 ;;; LSP
 (use-package lsp-mode
   :straight t
   :init
   (setq lsp-keymap-prefix "C-c l")
-  :hook ((c-mode . lsp)
-         (c++-mode . lsp)
-         (go-mode . lsp)
-         (haskell-mode . lsp)
-         (haskell-literate-mode . lsp)
-         (java-mode . lsp)
-         (js-mode . lsp)
-         (python-mode . lsp)
-         (rust-mode . lsp)
+  :hook ((prog-mode . lsp)
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
 
@@ -175,32 +158,51 @@
   :straight t
   :commands lsp-treemacs-errors-list)
 
-;;; Flycheck
-(use-package flycheck
-  :straight t
-  :init (global-flycheck-mode))
-
-;;; Smartparens
-(use-package smartparens-config
-  :straight smartparens
-  :config
-  (smartparens-global-mode t)
-  (show-smartparens-global-mode t))
-
-;;; ace-window
-(use-package ace-window
+;;; Zenburn
+(use-package zenburn-theme
   :straight t
   :config
-  (global-set-key (kbd "M-o") 'ace-window))
+  (load-theme 'zenburn t))
 
-;; Turn off alarms completely.
-(setq ring-bell-function 'ignore)
+;;; Dashboard
+(use-package dashboard
+  :straight t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-banner-logo-title "E + mofa + cs")
+  (setq dashboard-startup-banner 'logo)
+  (setq dashboard-center-content t)
+  (setq dashboard-items '((projects . 8)
+                          (recents . 8)))
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-set-navigator t)
+  (setq dashboard-set-init-info t))
+
+;;; powerline
+(use-package powerline
+  :straight t
+  :config
+  (powerline-default-theme))
+
+;; Place auto-save files to a dedicated directory.
+(setq backup-directory-alist
+      `(("." . ,(concat user-emacs-directory "backups"))))
+
+;; Replace highlighted text with what I type.
+(delete-selection-mode 1)
 
 ;; Keybindings to resize windows.
 (global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
+;; Use spaces instead of tabs when indenting.
+(setq-default indent-tabs-mode nil)
+
+;; Turn off alarms completely.
+(setq ring-bell-function 'ignore)
 
 ;; Display line numbers.
 (when (version<= "26.0.50" emacs-version)
@@ -211,13 +213,3 @@
 
 ;; Start Emacs in fullscreen mode.
 (add-hook 'emacs-startup-hook 'toggle-frame-fullscreen)
-
-;; Place auto-save files to a dedicated directory.
-(setq backup-directory-alist
-      `(("." . ,(concat user-emacs-directory "backups"))))
-
-;; Replace highlighted text with what I type.
-(delete-selection-mode 1)
-
-;; Use spaces instead of tabs when indenting.
-(setq-default indent-tabs-mode nil)
