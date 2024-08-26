c5653169c64c3a544a2c14a6885971cc8cc43632
Jiacheng Huang <huangjiacheng0709@outlook.com>
2024-08-26 13:16:53 +0800

    Remove `:init` with `rust-mode-treesitter-derive` setting

--- rust.el
+++ rust.el
@@ -1,8 +1,7 @@
 (use-package rust-mode
   :straight t
-  :init
-  (setq rust-mode-treesitter-derive t)
   :config
-  (add-to-list 'eglot-server-programs
-               '((rust-ts-mode rust-mode) .
-                 ("rust-analyzer" :initializationOptions (:check (:command "clippy"))))))
+  (with-eval-after-load 'eglot
+    (add-to-list 'eglot-server-programs
+                 '((rust-ts-mode rust-mode) .
+                   ("rust-analyzer" :initializationOptions (:check (:command "clippy")))))))
